006805571c08007acd55660342b5a4cd5f3fba7b
yvesisaacoliver <122495714+yvesisaacoliver@users.noreply.github.com>
2026-05-02 00:01:23 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className="hero-left">
           <h1 className="hero-title">SUA MARCA MERECE MAIS</h1>
           <p className="hero-subtitle">Conexão real entre sua marca e seu público.</p>
-          <button className="hero-button" onClick={() => window.open('https://wa.me/5511989517146', '_blank')}>Solicitar Proposta</button>
+          <button className="hero-button" onClick={() => window.open('https://wa.me/5511991998553', '_blank')}>Solicitar Proposta</button>
           <p className="hero-description">
             A Oliva une criação de marca, marketing, tecnologia e dados para gerar conexão real entre sua marca e seu público, fortalecendo presença, atraindo os clientes certos e transformando relacionamento em resultado.
           </p>
